e3aa595c50ecc376de1c6ee04b3064017ec50f38
Isabela <isarocha0012@gmail.com>
2026-06-16 23:12:40 -0300

    Book cards
    Books cards have a responsive grid design

--- javascript.js
+++ javascript.js
@@ -15,4 +15,40 @@ function addBookToLibrary(title, author, year, read) {
   return book;
 }
 
-const laranjaMecanica = addBookToLibrary('Laranja Mecanica', 'Anthony Burgess', 1980, 'read')
+function displayLibrary() {
+    const libraryDiv = document.querySelector('.library');
+    
+    myLibrary.forEach(book => {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('bookDiv');
+        libraryDiv.appendChild(bookDiv);
+
+
+        const bookTitle = document.createElement('h3');
+        bookTitle.classList.add('info');
+        bookDiv.appendChild(bookTitle);
+        bookTitle.textContent = `Title: ${book.title}`;
+
+        const bookAuthor = document.createElement('p');
+        bookAuthor.classList.add('info');
+        bookDiv.appendChild(bookAuthor);
+        bookAuthor.textContent = `Author: ${book.author}`;
+
+        const bookYear = document.createElement('p');
+        bookYear.classList.add('info');
+        bookDiv.appendChild(bookYear);
+        bookYear.textContent = `Year: ${book.year}`;
+    })
+}
+
+const laranjaMecanica = addBookToLibrary('Laranja Mecanica', 'Anthony Burgess', 1980, 'read');
+
+const oMundoDeSofia = addBookToLibrary('O Mundo de Sofia', 'Jostein Gaarder', 1980);
+
+const dune = addBookToLibrary('Dune', 'Frank Herbert', 1980);
+
+const duneMesiah = addBookToLibrary('Dune Messiah', 'Frank Herbert', 1981);
+
+const ei = addBookToLibrary('Ei! Tem Alguem Ai', 'Jostein Gaarner', 1980);
+
+displayLibrary();
