a228f24ea0f22dc9d3744b0a4f8256c15964aa05
Isabela <isarocha0012@gmail.com>
2026-06-17 17:21:51 -0300

    Add new book pop up
    Created a pop up form so the user can input a new book. The form uses a dialog tag. Still have to work on the style.
    It doesn`t have memory (when the page reloads the books added by the button get removed)

--- javascript.js
+++ javascript.js
@@ -16,7 +16,9 @@ function addBookToLibrary(title, author, year, read) {
 }
 
 function displayLibrary() {
-    const libraryDiv = document.querySelector('.library');
+    const libraryDiv = document.querySelector('.my-library');
+
+    libraryDiv.innerHTML = '';
     
     myLibrary.forEach(book => {
         const bookDiv = document.createElement('div');
@@ -50,5 +52,37 @@ const dune = addBookToLibrary('Dune', 'Frank Herbert', 1980);
 const duneMesiah = addBookToLibrary('Dune Messiah', 'Frank Herbert', 1981);
 
 const ei = addBookToLibrary('Ei! Tem Alguem Ai', 'Jostein Gaarner', 1980);
+
+//form to add new book
+const dialog = document.querySelector('.book-dialog');
+const addBtn = document.querySelector('.add');
+const cancelBtn = document.querySelector('#cancel-btn');
+const form = document.querySelector('.book-form');
+
+addBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+cancelBtn.addEventListener('click', () => {
+    dialog.close();
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const year = document.querySelector('#year').value;
+    const read = document.querySelector('#read').checked;
+
+    addBookToLibrary(title, author, year, read);
+
+    form.reset();
+    dialog.close();
+
+    displayLibrary();
+});
+
+console.log(myLibrary);
 
 displayLibrary();
